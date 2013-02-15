687cf7cc5f2fdc9cb430b5017f9ea7aaaed5cbd4
Brian Lemke <brian.m.lemke@gmail.com>
2013-02-14 22:02:08 -0600

    Make user email lowercase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
   has_secure_password
 
+  before_save { |user| user.email = user.email.downcase }
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
