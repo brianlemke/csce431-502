9d06f4d9bd74c9ee76d0bba0262de804a2aec454
brad <brad@brad-VirtualBox.(none)>
2013-02-17 00:42:00 -0600

    Modified test for index view for file upload

--- spec/views/posters/index.html.erb_spec.rb
+++ spec/views/posters/index.html.erb_spec.rb
@@ -19,7 +19,6 @@ describe "posters/index" do
   it "renders a list of posters" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "File".to_s, :count => 2
     assert_select "tr>td", :text => "Title".to_s, :count => 2
     assert_select "tr>td", :text => false.to_s, :count => 2
   end
