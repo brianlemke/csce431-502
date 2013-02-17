3da566b3acbdf34b34885e0095392072518b426c
brad <brad@brad-VirtualBox.(none)>
2013-02-17 00:48:51 -0600

    Display image on show action for poster

--- spec/views/posters/show.html.erb_spec.rb
+++ spec/views/posters/show.html.erb_spec.rb
@@ -12,7 +12,6 @@ describe "posters/show" do
   it "renders attributes in <p>" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/File/)
     rendered.should match(/Title/)
     rendered.should match(/false/)
   end
