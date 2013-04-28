027b60b8792f605e7c76364fee9578e1b8b24f16
Brian Lemke <brian.m.lemke@gmail.com>
2013-04-28 12:58:36 -0500

    Add most recent posters from subscribed organizations

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,19 @@
 module UsersHelper
 
-	def is_user?
-		current_account.is_a? User
-	end
+  def is_user?
+    current_account.is_a? User
+  end
+
+  def recent_posters(user)
+    posters = []
+    user.subscriptions.each do |subscription|
+      posters.concat subscription.organization.posters
+    end
+    posters.sort do |a, b|
+      return -1 if a.updated_at > b.updated_at
+      return 0 if a.updated_at == b.updated_at
+      return 1
+    end
+    posters[0, 20]
+  end
 end
