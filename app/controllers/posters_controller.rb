c98496ceb25121fc6d7951ab55da75e5a1160557
Brian Lemke <brian.m.lemke@gmail.com>
2013-04-28 19:16:08 -0500

    Make Poster#mainlist and Poster#show world-viewable

--- app/controllers/posters_controller.rb
+++ app/controllers/posters_controller.rb
@@ -9,42 +9,34 @@ class PostersController < ApplicationController
 
 #Eventually use this for main page and when searching
   def mainlist
-    if signed_in?
-      @organizations = []
-      if params.has_key?(:orgSearch) && params[:orgSearch] != ''
-        @organizations = Organization.find(:all, :conditions => ['name LIKE ?', "%#{params[:orgSearch]}%"])
-      else
-        @organizations = Organization.all
-      end
-      @posters = []
-      if params.has_key?(:dateSearch) && params[:dateSearch] != ''
-        @organizations.each do |org|
-          @posters += org.posters.find(:all, :conditions => ['event_start_date BETWEEN date(?) AND date(?, "+1 day") AND title LIKE ? AND tag LIKE ? AND description LIKE ?', "#{params[:dateSearch]}", "#{params[:dateSearch]}", "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
-        end
-      elsif params.has_key?(:orgSearch) && params[:orgSearch] != ''
-        @organizations.each do |org|
-          @posters += org.posters.find(:all, :conditions => ['title LIKE ? AND tag LIKE ? AND description LIKE ?', "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
-        end
-      elsif params.has_key?(:titleSearch) && params[:titleSearch] != ''
-        @posters = Poster.find(:all, :conditions => ['title LIKE ? AND tag LIKE ? AND description LIKE ?', "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
-      elsif params.has_key?(:descriptionSearch) && params[:descriptionSearch] != ''
-        @posters = Poster.find(:all, :conditions => ['tag LIKE ? AND description LIKE ?', "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
-      elsif params.has_key?(:tagSearch) && params[:tagSearch] != ''
-        @posters = Poster.find(:all, :conditions => ['tag LIKE ?', "%#{params[:tagSearch]}%"])
-      else
-        @posters = Poster.all
-      end
+    @organizations = []
+    if params.has_key?(:orgSearch) && params[:orgSearch] != ''
+      @organizations = Organization.find(:all, :conditions => ['name LIKE ?', "%#{params[:orgSearch]}%"])
     else
-      redirect_to signin_path
+      @organizations = Organization.all
+    end
+    @posters = []
+    if params.has_key?(:dateSearch) && params[:dateSearch] != ''
+      @organizations.each do |org|
+        @posters += org.posters.find(:all, :conditions => ['event_start_date BETWEEN date(?) AND date(?, "+1 day") AND title LIKE ? AND tag LIKE ? AND description LIKE ?', "#{params[:dateSearch]}", "#{params[:dateSearch]}", "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
+      end
+    elsif params.has_key?(:orgSearch) && params[:orgSearch] != ''
+      @organizations.each do |org|
+        @posters += org.posters.find(:all, :conditions => ['title LIKE ? AND tag LIKE ? AND description LIKE ?', "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
+      end
+    elsif params.has_key?(:titleSearch) && params[:titleSearch] != ''
+      @posters = Poster.find(:all, :conditions => ['title LIKE ? AND tag LIKE ? AND description LIKE ?', "%#{params[:titleSearch]}%", "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
+    elsif params.has_key?(:descriptionSearch) && params[:descriptionSearch] != ''
+      @posters = Poster.find(:all, :conditions => ['tag LIKE ? AND description LIKE ?', "%#{params[:tagSearch]}%", "%#{params[:descriptionSearch]}%"])
+    elsif params.has_key?(:tagSearch) && params[:tagSearch] != ''
+      @posters = Poster.find(:all, :conditions => ['tag LIKE ?', "%#{params[:tagSearch]}%"])
+    else
+      @posters = Poster.all
     end
   end
 
   def show
-    if signed_in?
-      @poster = Poster.find(params[:id])
-    else
-      redirect_to signin_path
-    end
+    @poster = Poster.find(params[:id])
   end
 
   def new
